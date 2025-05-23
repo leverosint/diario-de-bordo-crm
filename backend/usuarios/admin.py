44c4f4df05a73e85ae1a20564010df87a36777a5
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-05-23 12:11:42 -0300

    subindo sidebar

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -5,7 +5,8 @@ from .models import CustomUser, CanalVenda
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('username', 'first_name', 'email', 'tipo_user', 'canal', 'id_vendedor', 'is_staff')
+    list_display = ('username', 'first_name', 'email', 'tipo_user', 'id_vendedor', 'is_staff')
+
     
     fieldsets = UserAdmin.fieldsets + (
         ('Dados Comerciais', {
