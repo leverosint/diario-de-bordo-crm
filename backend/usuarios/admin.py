386cf164786855f3c2a0da8a7ef10c8faaa95ba9
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-05-23 12:17:03 -0300

    subindo sidebar

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -1,25 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, CanalVenda
+from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('username', 'first_name', 'email', 'tipo_user', 'id_vendedor', 'is_staff')
-
-    
+    list_display = ('username', 'first_name', 'email', 'tipo_user', 'canal', 'id_vendedor', 'is_staff')
     fieldsets = UserAdmin.fieldsets + (
         ('Dados Comerciais', {
-            'fields': ('tipo_user', 'canal', 'id_vendedor', 'primeiro_acesso', 'canais_permitidos')
+            'fields': ('tipo_user', 'canal', 'id_vendedor', 'primeiro_acesso')
         }),
     )
-
     add_fieldsets = UserAdmin.add_fieldsets + (
         ('Dados Comerciais', {
-            'fields': ('tipo_user', 'canal', 'id_vendedor', 'primeiro_acesso', 'canais_permitidos')
+            'fields': ('tipo_user', 'canal', 'id_vendedor', 'primeiro_acesso')
         }),
     )
-
-@admin.register(CanalVenda)
-class CanalVendaAdmin(admin.ModelAdmin):
-    list_display = ('nome',)
