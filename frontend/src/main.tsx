d0dc9ed628fbc9111c9f3e31b6d5a3e5741e8354
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-05-26 17:39:12 -0300

    subindo front novo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
-import '@mantine/core/styles.css'; // <-- adicione isso
-
+import '@mantine/core/styles.css';
 
 import './index.css';
 import { customTheme } from './styles/theme';
 import LoginPage from './pages/LoginPage';
-import Dashboard from './pages/Dashboard'; // você criará esse em seguida
+import Dashboard from './pages/Dashboard';
+import CadastroParceiro from './components/CadastroParceiro'; // <-- novo import
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -17,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/cadastro-parceiro" element={<CadastroParceiro />} /> {/* nova rota */}
         </Routes>
       </BrowserRouter>
     </MantineProvider>
