6c45839ed9c9bbf514ae4bf7a1321c79b38f2a81
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-05-27 11:02:14 -0300

    subindo back e front 4

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import CustomUser, CanalVenda, Parceiro
 from django.contrib.auth.admin import UserAdmin
 
+
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'tipo_user', 'id_vendedor', 'exibir_canais')
@@ -16,10 +17,40 @@ class CustomUserAdmin(UserAdmin):
         return ", ".join([c.nome for c in obj.canais_venda.all()])
     exibir_canais.short_description = 'Canais de Venda'
 
+
 @admin.register(Parceiro)
 class ParceiroAdmin(admin.ModelAdmin):
     list_display = ('codigo', 'parceiro', 'cidade', 'uf', 'canal_venda')
     search_fields = ('codigo', 'parceiro', 'cidade', 'consultor')
     list_filter = ('canal_venda', 'uf', 'classificacao')
+    readonly_fields = ('tm', 'recorrencia', 'total_geral')
+
+    fieldsets = (
+        ('Informações do Parceiro', {
+            'fields': (
+                'codigo',
+                'parceiro',
+                'classificacao',
+                'consultor',
+                'canal_venda',  # mostrado como "Unidade" no display abaixo
+                'cidade',
+                'uf',
+            )
+        }),
+        ('Faturamento Calculado (Somente Leitura)', {
+            'fields': (
+                'tm',
+                'recorrencia',
+                'total_geral',
+            )
+        }),
+    )
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        field = super().formfield_for_dbfield(db_field, **kwargs)
+        if db_field.name == 'canal_venda':
+            field.label = 'Unidade'
+        return field
+
 
 admin.site.register(CanalVenda)
