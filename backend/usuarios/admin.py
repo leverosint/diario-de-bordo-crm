0025e7d3c647231fdc54ad370721611828780427
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-05-27 10:54:33 -0300

    subindo back e front 2

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, CanalVenda
+from .models import CustomUser, CanalVenda, Parceiro
 from django.contrib.auth.admin import UserAdmin
 
 @admin.register(CustomUser)
@@ -15,5 +15,11 @@ class CustomUserAdmin(UserAdmin):
     def exibir_canais(self, obj):
         return ", ".join([c.nome for c in obj.canais_venda.all()])
     exibir_canais.short_description = 'Canais de Venda'
+
+@admin.register(Parceiro)
+class ParceiroAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'parceiro', 'cidade', 'uf', 'canal_venda')
+    search_fields = ('codigo', 'parceiro', 'cidade', 'consultor')
+    list_filter = ('canal_venda', 'uf', 'classificacao')
 
 admin.site.register(CanalVenda)
