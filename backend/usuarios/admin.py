e15524cd31457a0448d15b39e4127796aa4761b8
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-05-27 11:10:01 -0300

    subindo back e front 2

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -1,29 +1,23 @@
-from django.contrib import admin
-from .models import CustomUser, CanalVenda, Parceiro
-from django.contrib.auth.admin import UserAdmin
-
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'tipo_user', 'id_vendedor', 'exibir_canais')
-    fieldsets = UserAdmin.fieldsets + (
-        ('Informações Adicionais', {
-            'fields': ('tipo_user', 'id_vendedor', 'primeiro_acesso', 'canais_venda')
-        }),
-    )
-    filter_horizontal = ('canais_venda',)
-
-    def exibir_canais(self, obj):
-        return ", ".join([c.nome for c in obj.canais_venda.all()])
-    exibir_canais.short_description = 'Canais de Venda'
-
-
 @admin.register(Parceiro)
 class ParceiroAdmin(admin.ModelAdmin):
     list_display = ('codigo', 'parceiro', 'cidade', 'uf', 'canal_venda')
     search_fields = ('codigo', 'parceiro', 'cidade', 'consultor')
     list_filter = ('canal_venda', 'uf', 'classificacao')
-    readonly_fields = ('tm', 'recorrencia', 'total_geral')
+
+    def get_readonly_fields(self, request, obj=None):
+        if not obj:
+            return ('tm', 'recorrencia', 'total_geral')
+        readonly = ['tm', 'recorrencia', 'total_geral']
+        meses = [
+            'janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho',
+            'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro',
+            'janeiro_2', 'fevereiro_2', 'marco_2'
+        ]
+        for mes in meses:
+            valor = getattr(obj, mes, 0)
+            if valor and valor != 0:
+                readonly.append(mes)
+        return readonly
 
     fieldsets = (
         ('Informações do Parceiro', {
@@ -32,12 +26,19 @@ class ParceiroAdmin(admin.ModelAdmin):
                 'parceiro',
                 'classificacao',
                 'consultor',
-                'canal_venda',  # mostrado como "Unidade" no display abaixo
+                'canal_venda',
                 'cidade',
                 'uf',
             )
         }),
-        ('Faturamento Calculado (Somente Leitura)', {
+        ('Faturamento Mensal', {
+            'fields': (
+                'janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho',
+                'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro',
+                'janeiro_2', 'fevereiro_2', 'marco_2',
+            )
+        }),
+        ('Totais e Cálculos (Somente Leitura)', {
             'fields': (
                 'tm',
                 'recorrencia',
@@ -50,7 +51,11 @@ class ParceiroAdmin(admin.ModelAdmin):
         field = super().formfield_for_dbfield(db_field, **kwargs)
         if db_field.name == 'canal_venda':
             field.label = 'Unidade'
+        if db_field.name in [
+            'janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho',
+            'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro',
+            'janeiro_2', 'fevereiro_2', 'marco_2',
+            'tm', 'total_geral'
+        ]:
+            field.widget.attrs['style'] = 'width: 200px'
         return field
-
-
-admin.site.register(CanalVenda)
