200f6655e1d9609214dc04e8d592e3e52a8ad0ce
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-05-28 13:43:11 -0300

    aaaa

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,16 +3,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
-import InteracoesPage from './pages/InteracoesPage';
 
 import './index.css';
 import { customTheme } from './styles/theme';
 
 import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
-import CadastroParceiroPage from './pages/CadastroParceiroPage'; // ✅ novo
-import CadastroUsuariosPage from './pages/CadastroUsuariosPage'; // ✅ novo
-console.log('Componente Interações carregado');
+import CadastroParceiroPage from './pages/CadastroParceiroPage';
+import CadastroUsuariosPage from './pages/CadastroUsuariosPage';
+import Interacoes from './pages/Interacoes'; // ✅ import atualizado
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -23,7 +22,7 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/cadastro-parceiro" element={<CadastroParceiroPage />} />
           <Route path="/cadastro-usuarios" element={<CadastroUsuariosPage />} />
-                  <Route path="/interacoes" element={<InteracoesPage />} />
+          <Route path="/interacoes" element={<Interacoes />} /> {/* ✅ uso direto */}
         </Routes>
       </BrowserRouter>
     </MantineProvider>
