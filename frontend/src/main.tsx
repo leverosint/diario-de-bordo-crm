cca3c132be2d9272935fff5f162572bd3fc5173e
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-06-16 14:09:50 -0300

    teste de interacao

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,35 +1,29 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { MantineProvider, CssBaseline } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
-
-// Importação dos estilos do Mantine
 import '@mantine/core/styles.css';
-import '@mantine/dates/styles.css';
 
-import './index.css';
-
-// Importação do tema customizado
-import { customTheme } from './styles/theme';
-
-// Importação das páginas
+import Relatorios from './pages/Relatorios';
+import TabelaOportunidadesPage from './pages/TabelaOportunidadesPage';
 import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
 import CadastroParceiroPage from './pages/CadastroParceiroPage';
 import CadastroUsuariosPage from './pages/CadastroUsuariosPage';
 import InteracoesPage from './pages/InteracoesPage';
-import Relatorios from './pages/Relatorios';
-import TabelaOportunidadesPage from './pages/TabelaOportunidadesPage';
+
+import './index.css';
+import { customTheme } from './styles/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider
       theme={customTheme}
-      defaultColorScheme="light" // 🔥 Define se começa com tema claro ou escuro
+      defaultColorScheme="light"
+      withCssVariables
     >
-      <CssBaseline /> {/* 🔥 Essencial para normalizar os estilos */}
-      <ModalsProvider> {/* 🔥 Essencial para os Modals funcionarem */}
+      <ModalsProvider>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LoginPage />} />
