e4bc6c2ecf3d804f0b357b30a458d042a131de80
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-06-16 16:39:30 -0300

    socorro

--- backend/usuarios/views.py
+++ backend/usuarios/views.py
@@ -514,7 +514,7 @@ class DashboardFunilView(APIView):
             {"name": "Interações", "value": interacoes.count()},
             {"name": "Oportunidades", "value": oportunidades.filter(etapa='oportunidade').count()},
             {"name": "Orçamentos", "value": oportunidades.filter(etapa='orcamento').count()},
-            {"name": "Perdidas", "value": oportunidades.filter(etapa='perdida').count()},
+            {"name": "Perdidas", "value": oportunidades.filter(etapa='aguardando').count()},
             {"name": "Pedidos", "value": oportunidades.filter(etapa='pedido').count()},
             {"name": "Perdidas", "value": oportunidades.filter(etapa='perdida').count()},
             
