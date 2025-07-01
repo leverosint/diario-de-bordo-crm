4ee3721bf46d863d3ac45ad513c10effbe9a855b
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-07-01 01:15:34 -0300

    ajustando modelspy para conter id do gatilho, dessa forma cada gatilho rodara unicamente

--- backend/usuarios/models.py
+++ backend/usuarios/models.py
@@ -215,6 +215,8 @@ class GatilhoExtra(models.Model):
     parceiro = models.ForeignKey(Parceiro, on_delete=models.CASCADE, related_name='gatilhos_extras')
     usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='gatilhos_extras')
     descricao = models.CharField(max_length=255)
+    gatilho_utilizado = models.BooleanField(default=False)
+
 
     def __str__(self):
         return f'{self.parceiro.parceiro} - {self.usuario.username} ({self.descricao})'
