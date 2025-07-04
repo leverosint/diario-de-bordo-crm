491014b3e29b0104c3e68f683fa2b4856bd73b2b
Thiago Belchior de Camargo <thiago.camargo@multiar.com.br>
2025-07-04 12:20:37 -0300

    ajuste filé da tabela de vendedor e interação pendente

--- frontend/src/pages/InteracoesPage.tsx
+++ frontend/src/pages/InteracoesPage.tsx
@@ -1130,7 +1130,7 @@ export default function InteracoesPage() {
       <td>{item.status}</td>
       <td>{item.data_interacao ? new Date(item.data_interacao).toLocaleString() : ''}</td>
       <td>{item.tipo}</td>
-      <td>{item.usuario_nome}</td>
+      <td>{item.vendedor}</td> {/* ✅ aqui certinho */}
     </tr>
   ))}
 </tbody>
